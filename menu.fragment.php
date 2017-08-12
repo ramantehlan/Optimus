d6e0f3f5aed054a761378d2070c32e1804da7987
Raman Tehlan <ramantehlan@gmail.com>
2017-08-13 01:13:28 +0530

    navigation fixed

--- menu.fragment.php
+++ menu.fragment.php
@@ -1,42 +1,42 @@
 <div class="menu">
-		<a href="http://localhost/">
+		<a href="index.php">
 		<div class='logo'>
 				Optimus
 		</div>
 		</a>
 
 		<div class="options_holder">
-				<a href="dashboard" class='options_a'>
+				<a href="app?page=dashboard" class='options_a'>
 					<div class='option <?php if($page == "dashboard"){echo "selected_option";} ?>'>
 							<div class='option_img'>
-								<img src="http://localhost/assets/image/dashboard(2).png">
+								<img src="assets/image/dashboard(2).png">
 							</div>
 							Dashboard
 					</div>
 				</a>
 
-				<a href="analysis" class='options_a'>
+				<a href="app?page=analysis" class='options_a'>
 					<div class='option <?php if($page == "analysis"){echo "selected_option";} ?>'>
 							<div class='option_img'>
-								<img src="http://localhost/assets/image/analytics.png">
+								<img src="assets/image/analytics.png">
 							</div>
 							Analysis
 					</div>
 				</a>
 
-				<a href="data_upload" class='options_a'>
+				<a href="app?page=data_upload" class='options_a'>
 					<div class='option <?php if($page == "data_upload"){echo "selected_option";} ?>'>
 							<div class='option_img'>
-								<img src="http://localhost/assets/image/outbox.png">
+								<img src="assets/image/outbox.png">
 							</div>
 							Data Upload
 					</div>
 				</a>
 
-				<a href="image_upload" class='options_a'>
+				<a href="app?page=image_upload" class='options_a'>
 					<div class='option <?php if($page == "image_upload"){echo "selected_option";} ?>'>
 							<div class='option_img'>
-								<img src="http://localhost/assets/image/image(1).png">
+								<img src="assets/image/image(1).png">
 							</div>
 							SKU Upload
 					</div>
