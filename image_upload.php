f02339a19e408881d640dba5e2314502b5a3db9c
Raman Tehlan <ramantehlan@gmail.com>
2017-08-13 01:13:00 +0530

    navigation improved

--- image_upload.php
+++ image_upload.php
@@ -8,7 +8,7 @@
 			</style>
 
 			<div class="optimus_in_app">
-					<img src="http://localhost/assets/image/18134461ca57c4513277c0a134274dae.jpg">
+					<img src="assets/image/18134461ca57c4513277c0a134274dae.jpg">
 			</div>	
 
 <div class="welcome_heading effect_top_1">
@@ -25,7 +25,7 @@ Do you know i can see!
 		</div>
 		<div class="upload_box_body">
 
-				<form method="post" enctype="multipart/form-data" action="http://localhost/image_upload.action.php" id="submit_data_form">
+				<form method="post" enctype="multipart/form-data" action="image_upload.action.php" id="submit_data_form">
 
 
 						<input type="file" name="image" id="image" class="input">
