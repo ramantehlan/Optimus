a6bb688d6ae0b3c8087d0aa1054cfbb99ab2349d
Raman Tehlan <ramantehlan@gmail.com>
2017-08-13 01:21:47 +0530

    Bug Fixed

--- app.php
+++ app.php
@@ -39,10 +39,12 @@ date of creation:- 03/02/2017
 	<head>
 				<title>APP</title>
 
+				<link rel="stylesheet" href="assets/style/jquery-ui.css">
 				<link rel="stylesheet" href="assets/style/comman-ui.css">
 				<link rel="stylesheet" href="assets/style/app-ui.css">
 
 				<script type="text/javascript" src="assets/script/jquery.js"></script>
+				<script type="text/javascript" src="assets/script/jquery-ui.js"></script>
 				<script type="text/javascript" src="assets/script/effect.lib.js"></script>
 	</head>
 	<body>
